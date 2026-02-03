0c7fa459a7b16b1d21386201fd66f28fbf9aad9c
jayu66solanki <jayu66.solanki@gmail.com>
2026-02-03 19:36:24 +0530

    Update ArrayQueue.java

--- src/main/java/ArrayQueue.java
+++ src/main/java/ArrayQueue.java
@@ -7,51 +7,55 @@ public class ArrayQueue<E> implements QueueADT<E> {
 
     @SuppressWarnings("unchecked")
     public ArrayQueue(int capacity) {
-        if (capacity <= 0) throw new IllegalArgumentException("capacity must be > 0");
+        if (capacity <= 0)
+            throw new IllegalArgumentException("capacity must be > 0");
         this.data = (E[]) new Object[capacity];
     }
 
     @Override
     public int size() {
-        // TODO: return current number of elements
-        return -1;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        // TODO: return true if size == 0
-        return true;
+        return size == 0;
     }
 
     @Override
     public E first() {
-        // TODO: if empty return null; else return data[front]
-        return null;
+        if (isEmpty())
+            return null;
+        return data[front];
     }
 
     @Override
     public void enqueue(E e) {
-        Objects.requireNonNull(e, "Null elements are not supported in this assignment.");
+        Objects.requireNonNull(e, "Null elements are not supported.");
 
-        // TODO:
-        // 1) if full (size == data.length) throw IllegalStateException
-        // 2) compute available index = (front + size) % data.length
-        // 3) store e there
-        // 4) increment size
+        if (size == data.length)
+            throw new IllegalStateException("Queue is full");
+
+        int avail = (front + size) % data.length;
+        data[avail] = e;
+        size++;
     }
 
     @Override
     public E dequeue() {
-        // TODO:
-        // 1) if empty return null
-        // 2) store answer = data[front]
-        // 3) set data[front] = null (help GC)
-        // 4) front = (front + 1) % data.length
-        // 5) decrement size
-        // 6) return answer
-        return null;
+        if (isEmpty())
+            return null;
+
+        E answer = data[front];
+        data[front] = null;               // help GC
+        front = (front + 1) % data.length;
+        size--;
+
+        return answer;
     }
 
     // Helpful for debugging (not graded)
-    int capacity() { return data.length; }
+    int capacity() {
+        return data.length;
+    }
 }
